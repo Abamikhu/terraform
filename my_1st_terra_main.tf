2fa535fb51cbac82b95cb7700408f21c231d4241
Robert <89417860+Abamikhu@users.noreply.github.com>
2023-01-29 22:05:18 -0600

    Update my_1st_terra_main.tf

--- my_1st_terra_main.tf
+++ my_1st_terra_main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "vm" {
-  ami           = "DUMMY_VALUE_AMI_ID"
-  subnet_id     = "DUMMY_VALUE_SUBNET_ID"
+  ami           = "ami-007868005aea67c54"  
+  subnet_id     = "subnet-0409644c9b11f409b
   instance_type = "t3.micro"
   tags = {
     Name = "my-first-tf-node"
